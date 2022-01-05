4dfdc7a7fb2ec18a8f196d6a05ce9c574109dde3
kelia123 <atetek047@gmail.com>
2022-01-05 12:46:10 +0200

    initializing project

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv"
+import mongoose  from "mongoose";
+
+dotenv.config ("./.env");
+const app=express();
+
+app.use(bodyParser.json());
+
+
+app.use("/",(req,res)=> res.status(200).json({
+    message: "This is Tour APi"
+}));
+
+const dbUrl=process.env.DATABASEURL;
+
+mongoose.connect(dbUrl).then(()=> console.log("Database connected successfully"));
+
+const port=process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`Server is running on Port ${port}`);
+})
+
+export default app;
